8a051d0b0c86e79878a732fb8a3fc7e34277de92
Dat Le Thac <datquynhvinh1231@gmail.com>
2022-10-03 11:10:19 +0700

    set up babel plugin

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,16 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+// import images from '../';
+
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-            Header
+            <div className={cx('logo')}>
+                
+            </div>
         </div>
     </header>
 }
