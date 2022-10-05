e2be758d5fa89ae76a73870540eb4beff5f3a35d
Dat Le Thac <datquynhvinh1231@gmail.com>
2022-10-05 15:14:56 +0700

    useDebounce set delay search

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -12,6 +12,7 @@ import AccountItem from '../../../AccountItem';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '../../Popper';
 import styles from './Search.module.scss';
+import { useDebounce } from '../../../../hooks';
 
 const cx = classNames.bind(styles);
 
@@ -22,16 +23,17 @@ function Search() {
     const [loading, setLoading] = useState(false);
 
     const inputRef = useRef();
+    const debounced = useDebounce(searchValue, 500);
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then(res => res.json())
             .then(res => {
                 setSearchResult(res.data);
@@ -40,7 +42,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             })
-    }, [searchValue])
+    }, [debounced])
 
     const handleHideResult = () => {
         setShowResult(false);
